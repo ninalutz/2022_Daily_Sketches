ee609383b4049835637f1ff1691b19943f3a5c1d
ninalutz <nlutz@mit.edu>
2022-04-23 19:14:59 -0700

    add sketch

---
+++ sketches/4_23/sketch.js
@@ -0,0 +1,58 @@
+//forked from  kyami 
+
+let moverSystems = [];
+let num = 10;
+let cells, cols, rows;
+let offset, margin;
+let cellW, cellH;
+let bg;
+let graphics;
+
+
+function setup() {
+  w = 600;
+  var canvas;
+    if(windowWidth>=600 && windowHeight >=600){
+      canvas = createCanvas(600, 600);
+    }
+    else{
+      canvas = createCanvas(windowWidth, windowHeight - 100);
+    }
+
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+
+  canvas.position(x, y);
+  background(0);
+  noStroke();
+}
+
+
+function draw() {
+  let t = frameCount * 0.02;
+  randomSeed(3);
+  background(0);
+  for (let i=0; i < 3000; i++) {
+    let x = random(width); let y = random(width);
+    let r = width/2 - max(abs(x-width/2),abs(y-width/2));
+
+    for (let j=0; j < 12; j++) {
+      let sx = width/2+245*cos(-j*1.2+t);
+      let sy = width/2+245*sin(-j*0.52+t);
+      let s = 50;
+      if (x < width/2) {
+        r = min(r,dist(x,y,sx,sy)-s-20);
+      } else {
+          r = min(r, min(abs(sy-s-y),abs(sy+s-y))-1);
+      }
+    }
+
+    
+    r = min(r, abs(width/2-x));
+    
+    let c = color('#FFF9F9');
+    c.setAlpha(8*exp(-r/20.0));
+    fill(c);
+    ellipse(x,y,r*2, r*2);
+  }
+}
